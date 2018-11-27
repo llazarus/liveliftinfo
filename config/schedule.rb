9776e1937f21ed7bc8e88c4eb72290ebc5e9a136
Leland Lazarus <llazarus93@gmail.com>
2018-11-27 12:04:06 -0800

    TODO: add tasks created in last commit to 'schedule.rb'

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,19 +2,9 @@
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
+# 
+# Examples: http://github.com/javan/whenever
+#  
+### WHENEVER YOU ADD A JOB, IN TERMINAL RUN: "whenever --update-crontab" ###
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
+# TODO: Schedule jobs. After scheduling jobs REMEMBER TO RUN "whenever --update-crontab" !!!!!!!!
