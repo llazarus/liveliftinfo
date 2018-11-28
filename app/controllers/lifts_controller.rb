853f34ef9fcbecd3d2835a64f1115848a42dea3d
Leland Lazarus <llazarus93@gmail.com>
2018-11-28 13:21:03 -0800

    Created panels for B & W Mtns. Created sub-panels for the aforementioned to list all lifts by name within the relevant panel

--- app/controllers/lifts_controller.rb
+++ app/controllers/lifts_controller.rb
@@ -1,6 +1,8 @@
 class LiftsController < ApplicationController
   def index
     @lifts = Lift.all
+    @blackcomb_lifts = @lifts.slice(0, 15)
+    @whistler_lifts = @lifts.drop(15)
   end
 
   def show
