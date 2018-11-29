8eee239266c9315b60f091c249c5f99a175ad861
Leland Lazarus <llazarus93@gmail.com>
2018-11-28 21:30:54 -0800

    Rough draft of lifts index with status, weather, avy forecast

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def retrieve_lift_status(lift)
-    code = lift.lift_code
-     return Status.where(lift_code: code)
+  def retrieve_lift_status(lift) 
+    Status.where(lift_code: lift.lift_code).last.status
   end
   helper_method :retrieve_lift_status
+
+  def string_to_date(string)
+    DateTime.parse(string).strftime("%B %d, %Y")
+  end
+  helper_method :string_to_date
 
   protected
   def configure_permitted_parameters
--- app/controllers/lifts_controller.rb
+++ app/controllers/lifts_controller.rb
@@ -3,6 +3,8 @@ class LiftsController < ApplicationController
     @lifts = Lift.all
     @blackcomb_lifts = @lifts.slice(0, 15)
     @whistler_lifts = @lifts.drop(15)
+    @weather = Forecast.last
+    @avalanche = Avalanche.last
   end
 
   def show
