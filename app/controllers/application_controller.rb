967bb7722c647db17d58135bbf8259095b51a9bd
Leland Lazarus <llazarus93@gmail.com>
2018-12-03 21:59:26 -0800

    Went back to svg from png for smoother rendering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
     status = Status.where(lift_code: lift.lift_code).last.status.upcase
 
     if status == "O"
-      "/assets/status_icons/open.png"
+      "/assets/status_icons/open.svg"
     elsif status == "H"
-      "/assets/status_icons/hold.png"
+      "/assets/status_icons/hold.svg"
     else
-      "/assets/status_icons/closed.png"
+      "/assets/status_icons/closed.svg"
     end
   end
   helper_method :retrieve_lift_status
@@ -21,15 +21,15 @@ class ApplicationController < ActionController::Base
 
   def retrieve_lift_image(lift_type) 
     if lift_type == "T"
-      "/assets/lift_icons/rope_tow.png"
+      "/assets/lift_icons/rope_tow.svg"
     elsif lift_type == "3"
-      "/assets/lift_icons/three_p_chair.png"
+      "/assets/lift_icons/three_p_chair.svg"
     elsif lift_type == "4"
-      "/assets/lift_icons/four_p_chair.png"
+      "/assets/lift_icons/four_p_chair.svg"
     elsif lift_type == "6"
-      "/assets/lift_icons/six_p_chair.png"
+      "/assets/lift_icons/six_p_chair.svg"
     else
-      "/assets/lift_icons/gondola.png"
+      "/assets/lift_icons/gondola.svg"
     end
   end
   helper_method :retrieve_lift_image
@@ -52,21 +52,21 @@ class ApplicationController < ActionController::Base
   def retrieve_weather_image(weather_description)
     description = weather_description.downcase
     if description == "fairpartlycloudy" || description == "mostlysunny"
-      "/assets/weather_icons/partly_cloudy.png"
+      "/assets/weather_icons/partly_cloudy.svg"
     elsif description == "sunny"
-      "/assets/weather_icons/sunny.png"
+      "/assets/weather_icons/sunny.svg"
     elsif description == "cloudy" || description == "mostlycloudy"
-      "/assets/weather_icons/cloudy.png"
+      "/assets/weather_icons/cloudy.svg"
     elsif description == "rain"
-      "/assets/weather_icons/rain.png"
+      "/assets/weather_icons/rain.svg"
     elsif description == "showers"
-      "/assets/weather_icons/rain_showers.png"
+      "/assets/weather_icons/rain_showers.svg"
     elsif description == "snowshowers"
-      "/assets/weather_icons/snow_showers.png"
+      "/assets/weather_icons/snow_showers.svg"
     elsif description == "snow"
-      "/assets/weather_icons/snow.png"
+      "/assets/weather_icons/snow.svg"
     else
-      "/assets/weather_icons/weather_generic.png"
+      "/assets/weather_icons/weather_generic.svg"
     end
   end
   helper_method :retrieve_weather_image
