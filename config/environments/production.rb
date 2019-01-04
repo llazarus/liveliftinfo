f75e50e3fc120f13fac10cd58f90efd992267c97
Leland Lazarus <llazarus93@gmail.com>
2019-01-03 18:58:38 -0800

    more aws modifications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
   config.serve_static_assets = false
-
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -30,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
