222945ed3ebad1124e83680a6935cd4bb6cf2028
Leland Lazarus <llazarus93@gmail.com>
2019-01-03 18:38:44 -0800

    more aws modifications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  config.serve_static_assets = false
   
   # Settings specified here will take precedence over those in config/application.rb.
 
