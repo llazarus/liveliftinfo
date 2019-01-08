8809a11414e427cfdee3502198e6e977ea793d0f
Leland Lazarus <llazarus93@gmail.com>
2019-01-07 17:23:32 -0800

    aws setting changes for timezone

--- config.ru
+++ config.ru
@@ -3,10 +3,3 @@
 require_relative 'config/environment'
 
 run Rails.application
-
-if Rails.env.production?
-  DelayedJobWeb.use Rack::Auth::Basic do |email, password|
-    ActiveSupport::SecurityUtils.variable_size_secure_compare('email', email) &&
-      ActiveSupport::SecurityUtils.variable_size_secure_compare('password', password)
-  end
-end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = {
-    host: "http://liveliftinfo.us-west-2.elasticbeanstalk.com"
+    host: "www.liveliftinfo.com"
 }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
@@ -100,4 +100,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.time_zone = 'Pacific Time (US & Canada)'
+  config.active_record.default_timezone = :local
 end
