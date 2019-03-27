85f781c4d5c401f39fcd9b6f2170281c637a8f3d
Leland Lazarus <llazarus93@gmail.com>
2019-03-27 15:54:46 -0700

    Moved sms dispatch from out of delayed jobs

--- app/models/status.rb
+++ app/models/status.rb
@@ -6,7 +6,32 @@ class Status < ApplicationRecord
     if self.status.upcase == "O"
       lift_id = Lift.where(lift_code: self.lift_code).first.id
       
-      SendLiftAlertJob.perform_later(lift_id)
+      # Remove delay job due to AWS issues
+      # SendLiftAlertJob.perform_later(lift_id)
+
+      users_to_alert = Favorite.where({favoritable_type: "Alert", favoritable_id: lift_id})
+    
+      lift_name = Lift.find(lift_id).name
+  
+      if users_to_alert.length > 0
+        users_to_alert.each do |user|
+        notify_user = User.find(user.favoritor_id)
+  
+        # Send SMS to user
+        sms = Aws::SNS::Client.new(
+          region: ENV["AWS_REGION"],
+          access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+          secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"])
+  
+        sms.publish(phone_number: notify_user.phone, 
+                    message: "#{lift_name} is open! Full site: www.liveliftinfo.com")
+  
+        # Delete all user row from Favorite table
+        Favorite.where({favoritable_type: "Alert", favoritor_id: user.favoritor_id, favoritable_id: lift_id}).first.delete
+        end
+        puts "<<< Sent SMS Alerts For #{lift_name} >>> \n\n"
+      end  
+      puts "<<< No SMS Alerts Sent For #{lift_name} >>> \n\n"
     end 
   end
 
